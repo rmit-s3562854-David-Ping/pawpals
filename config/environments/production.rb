121bcc71c9883675eb9fa1cc846928f3def936c4
David Ping <s3562854@student.rmit.edu.au>
2019-10-09 11:03:50 +1100

    Updated credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
+  # Store uploaded files on Amazon S3 bucket
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
